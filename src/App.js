7dffdb80dd3011522cfd6730ab04f2092b40721b
Nico Dasanbiagio <ndasanbiagio@gmail.com>
2022-01-18 11:43:17 -0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
+  const sayName = () => alert('Acá podes encontrar todo sobre la Cerveza')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> Cerveza Amber</p>
+      <header >
+        <img src={logo} className="" alt="" />
+        <h1> Cerveza Amber</h1>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Próximamente...</a>
       </header>
+      <div>
+        <button onClick={sayName}>Comprar</button>
+        <br></br>
+        <input type='text' placeholder='Buscar...'></input>
+      </div>
+
     </div>
   );
 }
